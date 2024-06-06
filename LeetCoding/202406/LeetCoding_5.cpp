2a3d234dd3aa09d9dc3a789e17710d405dda30d9
MaJian-ShiPanPan <majian_panpan@163.com>
2024-06-07 06:54:03 +0800

    20240607 am

--- LeetCoding/202406/LeetCoding_5.cpp
+++ LeetCoding/202406/LeetCoding_5.cpp
@@ -4,6 +4,13 @@
  * 
  * 知识点：
  *  动态规划
+ *      状态：dp[l][r] ---> 表示从l到r上是否为回文数。
+ *      状态转移方程：
+ *          dp[l][r] = true; (dp[l + 1][r - 1] == true || r - l + 1 <= 2) && (s[l] == s[r])
+ *          dp[l][r] = false; else;
+ *      边界条件：
+ *          dp[i][i] = true;
+ *          
 */
 
 #include <iostream>
@@ -18,10 +25,11 @@ public:
         int iMaxLength = 1;
         int iStart = 0;
         int iEnd = 0;
-        for(int r = 0; r < s.size(); ++r)
+        for(int r = 1; r < s.size(); ++r)
         {
             for(int l = 0; l < r; ++l)
             {
+                dp[l][r] = false; // 需要赋值一下，不然就是随机值。
                 if((s[l] == s[r]) && ((r - l <= 2) || (dp[l + 1][r - 1] == true)))
                 {
                     dp[l][r] = true;
@@ -30,9 +38,11 @@ public:
                         iStart = l;
                         iEnd = r;
                         iMaxLength = r - l + 1;
-                        std::cout << iStart << " " << iEnd << " " << s.substr(iStart, iEnd) << std::endl;
+                        std::cout << iStart << " " << iEnd << " " << s.substr(iStart, iMaxLength) << std::endl;
                     }
                 }
+                std::cout << "l: " << l << ", r:" << r
+                    << ", s[l]: " << s[l] << ", s[r]: " << s[r] << ", dp[l][r]: " << dp[l][r] << std::endl; 
             }
         }
         return s.substr(iStart, iMaxLength);
@@ -42,7 +52,7 @@ public:
 int main(int argc, char* argv[])
 {
 
-    std::string s = "abacbcad";
+    std::string s = "aacabdkacaa";
     Solution solution;
     std::cout << solution.longestPalindrome(s) << std::endl;
     return 0;
