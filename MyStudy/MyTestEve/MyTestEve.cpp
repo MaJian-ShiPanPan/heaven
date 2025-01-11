88b36504b10291e26c31799dddb904cb91fb15e5
mac <majian_panpan@163.com>
2025-01-11 09:11:16 +0800

    12

---
+++ MyStudy/MyTestEve/MyTestEve.cpp
@@ -0,0 +1,9 @@
+//
+// Created by venus on 2025/1/11.
+//
+#include <iostream>
+int MyTestEve()
+{
+    std::cout << "mj" << std::endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "MyStudy/MyTestEve/MyTestEve.cpp"
+
+int main()
+{
+
+    MyTestEve();
+    return 0;
+}
